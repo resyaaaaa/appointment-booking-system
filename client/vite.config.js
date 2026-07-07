ab68ee2033c75eb4dfecc6a21c945282357b5753
rurururiri <165371710+resyaaaaa@users.noreply.github.com>
2026-07-08 00:20:34 +0800

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,8 +13,11 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
+     proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
   }
 })
